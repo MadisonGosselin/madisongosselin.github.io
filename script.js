0031e60634126f2468bfb7a2546ef84ee16ff656
Madison Gosselin <madisoncgosselin@gmail.com>
2023-09-13 20:28:50 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//open&close menu
+function openNav(){
+    document.getElementById("nav").style.width = "35%";
+}
+
+function closeNav(){
+    document.getElementById("nav").style.width = "0%"
+}
+
