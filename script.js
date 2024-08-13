67bc2a46d877a082529ffd89fe885dd54aeae9cd
Madison Gosselin <86690087+MadisonGosselin@users.noreply.github.com>
2024-08-13 18:16:18 +0000

    started on light and dark mode

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 
+// create instances of needed html elements
+const body = document.querySelector("body")
+const modeSelector = document.querySelector(".modeSelector")
+
+console.log(localStorage.getItem("storedMode"))
+let state = localStorage.getItem("storedMode") // create item in local storage to keep last mode
+
+const loadDisplay = () => {
+    if(state == "light") {
+        modeSelector.innerText = "🖤"
+        body.classList.remove("darkMode")
+    } else {
+        modeSelector.innerText = "🤍"
+        body.classList.add("darkMode")
+    }
+}
+loadDisplay()
+
+modeSelector.addEventListener("click", (e) => {
+    if(state == "light") {
+        localStorage.setItem("storedMode", "dark")
+        body.classList.add("darkMode")
+        modeSelector.innerText = "🤍"
+    } else {
+        localStorage.setItem("storedMode", "light")
+        body.classList.remove("darkMode")
+        modeSelector.innerText = "🖤"
+    }
+    state = localStorage.getItem("storedMode")
+})
 
 document.querySelector(".openMenu").addEventListener("click", (e) => {
-    document.getElementById("nav").style.width = "35%";
+    document.getElementById("nav").style.width = "35%"
 })
 
 document.querySelector(".closebtn").addEventListener("click", (e) => {
